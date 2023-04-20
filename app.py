1c6aa91236b7a5f2bf8e45d59a42c11fe32c7a99
Your Name <you@example.com>
2023-04-20 19:45:19 +0800

    Your commit message here

--- app.py
+++ app.py
@@ -1,80 +1,64 @@
-
 from flask import Flask, render_template, request, redirect, url_for
 import pickle
 
 def generate_cytoscape_js(elements):
     nodes = [
-        "{ data: { id: '%s' } }" % node.replace("'", r"\'")
+        "{ data: { id: '%s' } }" % node
         for node in set(edge["source"] for edge in elements) | set(edge["target"] for edge in elements)
     ]
     edges = [
         "{ data: { id: 'edge%s', source: '%s', target: '%s', interaction: '%s' } }" % (
             i,
-            edge['source'].replace("'", r"\'"),
-            edge['target'].replace("'", r"\'"),
-            edge['interaction'].replace("'", r"\'"),
+            edge['source'],
+            edge['target'],
+            edge['interaction'],
         )
         for i, edge in enumerate(elements)
     ]
 
-    script = f"""
-    document.addEventListener('DOMContentLoaded', function () {{
-      const cy = cytoscape({{
-        container: document.getElementById('cy'),
-    
-        elements: [
-          // Nodes
-          {', '.join(nodes)},
-    
-          // Edges
-          {', '.join(edges)}
-        ],
-    
-        style: [
-          {{
-            selector: 'node',
-            style: {{
-              'background-color': '#6FB1FC', // Node background color
-              'label': 'data(id)',
-              'color': '#000000', // Node label text color
-              'text-opacity': 1, // Node label text opacity (0-1)
-              'font-size': '6px',
-              'text-halign': 'center',
-              'text-valign': 'center',
-              'text-wrap': 'wrap',
-              'text-max-width': 50
-            }},
-          }},
-    
-          {{
-            selector: 'edge',
-            style: {{
-              'width': 2,
-              'line-color': '#A9A9A9', // Edge line color
-              'curve-style': 'bezier', // Edge curve style
-              'target-arrow-color': '#ccc',
-              'target-arrow-shape': 'triangle',
-              'curve-style': 'bezier',
-              'label': 'data(interaction)',
-              'font-size': '6px',
-              'text-wrap': 'wrap', // Enable edge label text wrapping
-              'text-max-width': 50, // Maximum edge label text width before wrapping (in pixels)
-              'text-rotation': 'autorotate',
-              'text-margin-x': '5px',
-              'text-margin-y': '-5px'
-            }},
-          }},
-        ],
-    
-        layout: {{
-          name: 'cose'
-        }}
-      }});
+    a = open('network.txt','r').read()
 
-    }}
-    );
-    """
-    return script
+    nodes = ', '.join(nodes)
+    edges = ', '.join(edges)
+    return a.replace('NODES',nodes).replace('EDGES',edges)
+
+def process_network(elements):    
+    #remove redundancies
+    edges = []
+    for i in elements:
+        if i not in edges:
+            edges.append(i)
+
+    #groups similar nodes that have same source+interaction
+    edgeTypes = {}
+    for i in elements:
+        key = (i['source'], i['interaction'])
+        if key in edgeTypes:
+            edgeTypes[key] += [i['target']]
+        else:
+            edgeTypes[key] = [i['target']]
+    print(edgeTypes)
+    return edges
+
+def find_terms(my_search, genes):   
+    forSending = []
+    elements = [] 
+
+    if len(my_search)>0:
+        for i in genes:
+            if '!' in my_search: #exact search
+                if my_search.upper().strip().replace('!','') == i.strip():
+                    for j in genes[i]:
+                        if j[0]!='' and j[2]!='':
+                            forSending.append(Gene(j[0], j[2], j[1], j[3])) #source, target, type
+                            elements.append({"source": j[0].replace("'","").replace('"',''), "target": j[2].replace("'","").replace('"',''), "interaction": j[1].replace("'","").replace('"','')})                
+            if my_search.upper().strip() in i.strip():
+                print(i,'asd')
+                for j in genes[i]:
+                    if j[0]!='' and j[2]!='':
+                        forSending.append(Gene(j[0], j[2], j[1], j[3])) #source, target, type
+                        elements.append({"source": j[0].replace("'","").replace('"',''), "target": j[2].replace("'","").replace('"',''), "interaction": j[1]})
+    return elements, forSending
 
 app = Flask(__name__)
 
@@ -100,35 +84,46 @@ def search():
     except:
         my_search='cesa'
          
-    forSending = []
-    elements = []
-    
     if len(my_search)>0:
-        for i in genes:
-            if my_search.upper() in i:
-                for j in genes[i]:
-                    if j[0]!='' and j[2]!='':
-                        forSending.append(Gene(j[0], j[2], j[1], j[3])) #source, target, type
-                        elements.append({"source": j[0], "target": j[2], "interaction": j[1]})
+        split_search = my_search.split(';')
+        
+        forSending = []
+        elements = []    
+        for term in split_search:
+            results = find_terms(term, genes)
+            elements += results[0]
+            forSending += results[1]
 
-    cytoscape_js_code = generate_cytoscape_js(elements)
+
+        elements = process_network(elements)
+        cytoscape_js_code = generate_cytoscape_js(elements)
     if forSending!=[]:
         return render_template('gene.html', genes=forSending, cytoscape_js_code=cytoscape_js_code)
     else:
         return render_template('not_found.html')
 
 if __name__ == '__main__':
-    with open('allDic', 'rb') as file:
-        genes = pickle.load(file)
-    items, papers = 0, []
-    for i in genes:
-        items+=1
-        for j in genes[i]:
-            papers.append(j[3])
-    papers = set(papers)    
-    print(papers)
+    import os
+    items, edges = [],0
+    for i in os.listdir(os.getcwd()+'/annotations/'):
+        a = open(os.getcwd()+'/annotations/'+i,'r').read()
+        
+        if len(a)>0:
+            findings = a.split('\n\n')[1].split('\n')
+            
+            for j in findings:
+                if j.count('!')==2:
+                    splitta = j.split('!')
+        
+                    agentA, _, agentB = splitta
+                    agentA = agentA.split(':')[0].upper()
+                    agentB = agentB.strip().upper()
+                    edges+=1
+                    items+=[agentA]
+                    items+=[agentB]
+                
     
     v = open('stats.txt','w')
-    v.write(str(len(papers))+'\t'+str(items))
+    v.write(str(len(os.listdir(os.getcwd()+'/annotations/')))+'\t'+str(len(set(items))))
     v.close()
     app.run(debug=True)
